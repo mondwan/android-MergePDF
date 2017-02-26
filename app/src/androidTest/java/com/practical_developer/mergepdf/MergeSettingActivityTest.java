4988bc68a792b68c16f3142a038afc46947a05e9
Mondwan <victor2wen@gmail.com>
2017-02-26 21:01:08 +0800

    Verify the intent from the add source button

--- app/src/androidTest/java/com/practical_developer/mergepdf/MergeSettingActivityTest.java
+++ app/src/androidTest/java/com/practical_developer/mergepdf/MergeSettingActivityTest.java
@@ -1,6 +1,10 @@
 package com.practical_developer.mergepdf;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.ClipData;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -15,13 +19,23 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCategories;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasType;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 /**
@@ -29,10 +43,14 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class MergeSettingActivityTest {
+
+    String PDF_ONE = "one.pdf";
+    String PDF_TWO = "two.pdf";
+
     // Navigation from main activity is required for this activity
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule =
-        new ActivityTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> mIntentsRule =
+        new IntentsTestRule<>(MainActivity.class);
 
     @Before
     public void switchToMergeSetting() throws Exception {
@@ -44,6 +62,53 @@ public class MergeSettingActivityTest {
     public void getDefaultFileName() throws Exception {
         // TODO
     }
+
+    private Instrumentation.ActivityResult pickPDF(String f) {
+        return null;
+    }
+
+    @Test
+    public void showFileDialog() throws Exception {
+        // By default Espresso Intents does not stub any Intents. Stubbing
+        // needs to be setup before every test run. In this case all external
+        // Intents will be blocked.
+        intending(not(isInternal())).respondWith(
+            new Instrumentation.ActivityResult(
+                Activity.RESULT_CANCELED,
+                null
+            )
+        );
+
+        // Click the add file source button
+        onView(withId(R.id.addSource)).perform(click());
+
+        // Verify there is intent for selecting a file browser
+        // and there is an intent for select a pdf
+        intended(allOf(
+            hasAction(Intent.ACTION_CHOOSER),
+            hasExtra(
+                is(Intent.EXTRA_INTENT),
+                allOf(
+                    hasAction(Intent.ACTION_GET_CONTENT),
+                    hasType("application/pdf"),
+                    hasCategories(hasItem(equalTo(Intent.CATEGORY_OPENABLE)))
+                )
+            ),
+            hasExtra(Intent.EXTRA_TITLE, "Select a PDF")
+        ));
+    }
+
+//    @Test
+//    public void addPDFSource() throws Exception {
+//        // @TODO
+//        // Stub Intent for GET_CONTENT
+//        intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(
+//            pickPDF(PDF_ONE)
+//        );
+//
+//        // Click add file
+//        // Verify the contents
+//    }
 
     @Test
     public void testEmptyFileListByDefault() throws Exception {
